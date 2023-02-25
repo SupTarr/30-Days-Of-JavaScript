ba09ddbacb1af1841d0d11190ce46865b7dc4994
Supakrit Chuchatwannakul <tatasuppakrit@gmail.com>
2023-02-25 10:06:48 +0700

    feat: prettier to format code

--- 01_Day_Introduction/01_day_starter/main.js
+++ 01_Day_Introduction/01_day_starter/main.js
@@ -6,6 +6,6 @@
 
 /* Comments can make code readable, easy to reuse and informative */
 
-console.log(firstName, lastName, country, city, age, isMarried) 
-console.log(gravity, boilingPoint, PI) // 9.81, 100, 3.14
-console.log(name, job, live)
+console.log(firstName, lastName, country, city, age, isMarried);
+console.log(gravity, boilingPoint, PI); // 9.81, 100, 3.14
+console.log(name, job, live);
--- 02_Day_Data_types/primitive_data_types.js
+++ 02_Day_Data_types/primitive_data_types.js
@@ -1,14 +1,18 @@
-let word = 'JavaScript'
-// we dont' modify string 
-// we don't do like this, word[0] = 'Y' 
-let numOne = 3
-let numTwo = 3
-console.log(numOne == numTwo)      // true
+let word = "JavaScript";
+// We don't modify string
+// We don't do like this, word[0] = 'Y'
 
-let js = 'JavaScript'
-let py = 'Python'
-console.log(js == py)             //false 
+let numOne = 3;
+let numTwo = 3;
 
-let lightOn = true
-let lightOff = false
-console.log(lightOn == lightOff) // false
+console.log(numOne == numTwo); // true
+
+let js = "JavaScript";
+let py = "Python";
+
+console.log(js == py); //false
+
+let lightOn = true;
+let lightOff = false;
+
+console.log(lightOn == lightOff); // false
